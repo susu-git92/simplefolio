0a3aab20d3a043e3e00ffc42c6c9309cbbff5e23
susu-git92 <chlojeong@gmail.com>
2020-10-20 00:39:17 +0900

    Updates

---
+++ precache-manifest.277d436ffb3d98e9eca82e7ba0b35b59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc6abb4fedd02416171f940b36ddc4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6968af49d02d88dd3667",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "588787207a60c7391211",
+    "url": "/static/js/2.88e3a0b0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.88e3a0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6968af49d02d88dd3667",
+    "url": "/static/js/main.1d12c76c.chunk.js"
+  },
+  {
+    "revision": "f8a96825bb2e2aa86ffd",
+    "url": "/static/js/runtime-main.81ed67aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
